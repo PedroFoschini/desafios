d58b27595fff6e41b0243745297cad26be8933bd
Pedro Foschini <plfoschini@gmail.com>
2023-05-31 20:12:13 -0300

    Obtem valores do usuario e limite de loop

--- fibonacci.dart
+++ fibonacci.dart
@@ -1,28 +1,59 @@
+import 'dart:io';
+
 main(){
 
-  // Caso queira receber os valores iniciais do usuário, alterar apenas valorInicial 1 e 2
-  var valorInicial1 = 0;
-  var valorInicial2 = 1;
+  // Obter valores do usuário, se forem inválidos utiliza valores default
 
-  print('Fibonacci iniciando com $valorInicial1 e $valorInicial2:');
+  print('Informe o primeiro número inteiro da sequência: ');
+  var valorInicial1 = stdin.readLineSync();
 
-  fibonacci(primeiroValor: valorInicial1, segundoValor: valorInicial2);
+  print('Informe o segundo número inteiro da sequência: ');
+  var valorInicial2 = stdin.readLineSync();
+
+  print('Informe o número de repetições da sequência: ');
+  var stringRepeticoes = stdin.readLineSync();
+
+  int n1;
+  int n2;
+  int reps;
+
+  if(valorInicial1 is String && valorInicial1.length > 0){
+    n1 = int.parse(valorInicial1);
+  } else {
+    n1 = 0;
+  }
+
+  if(valorInicial2 is String && valorInicial2.length > 0){
+    n2 = int.parse(valorInicial2);
+  } else {
+    n2 = 1;
+  }
+
+  if(stringRepeticoes is String && stringRepeticoes.length > 0){
+    reps = int.parse(stringRepeticoes);
+  } else {
+    reps = 10;
+  }
+  
+  print('Fibonacci iniciando com $n1 e $n2:');
+
+  fibonacci(primeiroValor: n1, segundoValor: n2, repeticoes: reps);
 
 }
 
-void fibonacci({required int primeiroValor, required int segundoValor, bool executou = false}){
-
-  int soma = primeiroValor + segundoValor;
+void fibonacci({int primeiroValor = 0, int segundoValor = 1, bool executou = false, int repeticoes = 10}){
 
   if(!executou){
     print(primeiroValor);
     print(segundoValor);
   }
+  
+  int soma = primeiroValor + segundoValor;
 
   print(soma);
 
-  if(soma < 9000){
-    fibonacci(primeiroValor: segundoValor, segundoValor: soma, executou: true);
+  if(repeticoes > 1){
+    fibonacci(primeiroValor: segundoValor, segundoValor: soma, executou: true, repeticoes: --repeticoes);
   }
   
 }
