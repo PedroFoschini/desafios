8b21a7f9dedc9091d9a7ca11a4ea9a726ffd3296
Pedro Foschini <plfoschini@gmail.com>
2023-05-31 14:17:51 -0300

    Refatoração para imprimir os valores iniciais

--- fibonacci.dart
+++ fibonacci.dart
@@ -6,20 +6,23 @@ main(){
 
   print('Print de fibonacci iniciando com $valorInicial1 e $valorInicial2:');
 
-  print(valorInicial1);
-  print(valorInicial2);
-  fibonacci(primeiroValor: valorInicial1, segundoValor: valorInicial2);
+  fibonacci(primeiroValor: valorInicial1, segundoValor: valorInicial2, jaExecutou: false);
 
 }
 
-void fibonacci({required int primeiroValor, required int segundoValor}){
+void fibonacci({required int primeiroValor, required int segundoValor, required bool jaExecutou}){
 
   int soma = primeiroValor + segundoValor;
+
+  if(!jaExecutou){
+    print(primeiroValor);
+    print(segundoValor);
+  }
 
   print(soma);
 
   if(soma < 9000){
-    fibonacci(primeiroValor: segundoValor, segundoValor: soma);
+    fibonacci(primeiroValor: segundoValor, segundoValor: soma, jaExecutou: true);
   }
   
 }
