9f24b2b393b6b0a996968270c45afa4c679ca373
Pedro Foschini <plfoschini@gmail.com>
2023-06-02 17:11:28 -0300

    Adequação parâmetros e loop obter resposta

--- regraTres.dart
+++ regraTres.dart
@@ -12,59 +12,69 @@ main() {
   print('Informe o primeiro valor da relação desconhecida');
   var terceiraResposta = stdin.readLineSync();
 
-  print('A relação é diretamente proporcional? Digite S/N');
-  var quartaResposta = stdin.readLineSync();
+  var quartaResposta;
+  var repetir = true;
+  while(repetir){
+    print('A relação é diretamente proporcional? Digite S/N');
+    quartaResposta = stdin.readLineSync()!;
+    if(quartaResposta.toUpperCase() == 'N'){
+      repetir = false;
+    } else if(quartaResposta.toUpperCase() == 'S'){
+      repetir = false;
+    }
+  };
 
   var r1;
   if(primeiraResposta is String && primeiraResposta.length > 0){
     r1 = double.parse(primeiraResposta);
-  } else{
-    r1 = 0;
   }
 
   var r2;
   if(segundaResposta is String && segundaResposta.length > 0){
     r2 = double.parse(segundaResposta);
-  } else{
-    r2 = 0;
   }
 
   var r3;
   if(terceiraResposta is String && terceiraResposta.length > 0){
     r3 = double.parse(terceiraResposta);
-  } else{
-    r3 = 0;
   }
 
   var r4;
-  if(quartaResposta is String && quartaResposta.length > 0 && quartaResposta.toUpperCase() == 'S'){
+  if(quartaResposta.toUpperCase() == 'S'){
     r4 = true;
   } else{
     r4 = false;
   }
 
-  
-
-  regraTres(primeiroParam: r1, segundoParam: r2, terceiroParam: r3, diretamente: r4);
+  try{
+    print(regraTres(primeiroParam: r1, segundoParam: r2, terceiroParam: r3, diretamente: r4));
+  } catch (Exception){
+    print(Exception);
+  }
 
 }
 
-void regraTres(
-    {required num primeiroParam,
-    required num segundoParam,
-    required num terceiroParam,
-    required bool diretamente}) {
+double regraTres(
+    {num? primeiroParam,
+    num? segundoParam,
+    num? terceiroParam,
+    bool diretamente = true}) {
   var relacaoConhecida;
   var resultado;
+
+  if(primeiroParam == null || segundoParam == null || terceiroParam == null){
+    throw Exception('São necessários 3 valores para utilizar a regra de três!!!');
+  }
+
   // caso diretamente proporcional
   if (diretamente) {
     relacaoConhecida = segundoParam * terceiroParam;
     resultado = relacaoConhecida / primeiroParam;
-    print(resultado);
+    return resultado;
   } else {
     // caso inversamente proporcional
     relacaoConhecida = primeiroParam * segundoParam;
     resultado = relacaoConhecida / terceiroParam;
-    print(resultado);
+    return resultado;
   }
 }
